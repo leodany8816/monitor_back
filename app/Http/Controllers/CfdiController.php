66989649eba31403e6a033e70a86c2fd42473925
Leonardo Daniel Carranza García <leodany8816@gmail.com>
2024-09-21 19:50:48 -0600

    Se crea el metodo y la ruta para generar el arcivo zip y codificarlo a base64

--- app/Http/Controllers/CfdiController.php
+++ app/Http/Controllers/CfdiController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use ZipArchive;
+use Illuminate\Support\Facades\File;
 use App\Models\Factura;
 use app\Models\Usuario;
 use App\Models\Empresa;
+use Exception;
+use Illuminate\Support\Facades\Date;
 
 class CfdiController extends Controller
 {
@@ -27,16 +31,16 @@ class CfdiController extends Controller
                  * Formateamos la fecha a dd/mm/yy
                  */
                 $date = explode("T", $cfdi->comprobante_fecha);
-                $newDate = explode("-",$date[0]);
-                $fecha = $newDate[2]."-".$newDate[1]."-".$newDate[0];
+                $newDate = explode("-", $date[0]);
+                $fecha = $newDate[2] . "-" . $newDate[1] . "-" . $newDate[0];
 
                 /**
                  * en el tipo de comprobante si es "E" es Egreso si es "I" es ingreso
                  */
-                if($cfdi->comprobante_TipoDeComprobante == 'E')
-                    $tipoCom="Egreso";
+                if ($cfdi->comprobante_TipoDeComprobante == 'E')
+                    $tipoCom = "Egreso";
                 else
-                    $tipoCom="Ingreso";
+                    $tipoCom = "Ingreso";
 
                 return [
                     'id_factura' => $cfdi->id_factura,
@@ -48,10 +52,10 @@ class CfdiController extends Controller
                     'receptorRfc' => $cfdi->receptor_Rfc,
                     'fechaEmision' => $fecha,
                     'tipoComprobante' => $tipoCom,
-                    'subtotal' => "$".number_format($cfdi->comprobante_SubTotal,2),
-                    'traslado' => "$".number_format($cfdi->impuesto_trasladado,2),
-                    'retencion' => "$".number_format($cfdi->impuesto_retenido,2),
-                    'total' => "$".number_format($cfdi->comprobante_Total,2),
+                    'subtotal' => "$" . number_format($cfdi->comprobante_SubTotal, 2),
+                    'traslado' => "$" . number_format($cfdi->impuesto_trasladado, 2),
+                    'retencion' => "$" . number_format($cfdi->impuesto_retenido, 2),
+                    'total' => "$" . number_format($cfdi->comprobante_Total, 2),
                     'nombreXml' => $cfdi->nombre_xml,
                     'nombrePdf' => $cfdi->nombre_pdf,
                 ];
@@ -66,8 +70,8 @@ class CfdiController extends Controller
                 $cfdis
             );
         } else {
-            $jsonError =[
-               'exito' => 'false',
+            $jsonError = [
+                'exito' => 'false',
                 'message' => "No existen CFDIS"
             ];
 
@@ -75,5 +79,80 @@ class CfdiController extends Controller
                 $jsonError
             );
         }
+    }
+
+    public function downloadzip(Request $request)
+    {
+        // echo "metodo para descargar los zips";
+        // echo "<br>" . $request->cfdis;
+        $dataFiles = explode(',', $request->cfdis);
+        // echo "<br> tamanio " . count($dataFiles);
+
+        //Ruta donde se encuentran los archivos a buscar
+        $folderPath = storage_path('/app/public/cfdis/');
+
+        // Verificar si la carpeta existe
+        if (!File::exists($folderPath)) {
+            return response()->json(['message' => 'Folder not found'], 404);
+        }
+
+        // Crear un archivo ZIP temporal
+        // $zipFileName = 'cfdis->' . date('Ymd:Hms') . '.zip';
+        $zipFileName = 'cfdis_' . date('Ymd_His') . '.zip';
+        $zipPath = storage_path('/app/public/temp/' . $zipFileName);
+        $zip = new ZipArchive;
+
+        try {
+            if ($zip->open($zipPath, ZipArchive::CREATE) === TRUE) {
+                foreach ($dataFiles as $file) {
+                    $cfdi = Factura::findOrFail($file);
+                    $id_factura = $cfdi->id_factura;
+                    $fileXml = $cfdi->nombre_xml;
+                    $filePdf = $cfdi->nombre_pdf;
+                    // echo $id_factura . "|" . $fileXml . "|" . $filePdf;
+                    // echo "<br/>";
+                    // echo "listo";
+                    $filePathXml = $folderPath . $fileXml;
+                    $filePathPdf = $folderPath . $filePdf;
+                    if (file_exists($filePathXml) || file_exists($filePathPdf)) {
+                        // Agregar archivo al ZIP
+                        $zip->addFile($filePathXml, $fileXml);
+                        $zip->addFile($filePathPdf, $filePdf);
+
+                        // Cerrar el archivo ZIP
+                    } else {
+                        return response()->json([
+                            'exito' => false,
+                            'error' => "El archivo {$fileXml} {$filePdf} no se encontró."
+                        ], 404);
+                    }
+                }
+                $zip->close();
+            } else {
+                return response()->json([
+                    'exito' => false,
+                    'error' => 'No se pudo crear el archivo ZIP.'
+                ], 500);
+            }
+        } catch (Exception $error) {
+            return response()->json([
+                'exito' => false,
+                'error' => 'Error al crear el archivo ZIP.'
+            ], 500);
+        }
+
+        // Leer el archivo ZIP y codificarlo en base64
+        $zipContent = file_get_contents($zipPath);
+        $zipBase64 = base64_encode($zipContent);
+
+        // Eliminar el archivo ZIP después de codificarlo
+        File::delete($zipPath);
+
+        // Enviar la respuesta en formato JSON con el archivo ZIP codificado en base64
+        return response()->json([
+            'exito' => true,
+            'nombreZip' => $zipFileName,
+            'file_base64' => $zipBase64
+        ], 200);
     }
 }
