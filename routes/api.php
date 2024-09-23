b49f69904a3f3b27c4ac940bcd4d4f243cbfda37
Leonardo Daniel Carranza García <leodany8816@gmail.com>
2024-09-23 16:09:13 -0600

    Se desarrolla el metodo para descargar el archivo pdf

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,10 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 
 Route::post('/cfdi', [CfdiController::class, 'index'])->middleware('auth:sanctum');
 Route::post('/downloadzip',[CfdiController::class, 'downloadzip'])->middleware('auth:sanctum');
+Route::post('/downloadpdf', [CfdiController::class, 'downloadpdf'])->middleware('auth:sanctum');
 
 Route::post('/encabezado', [EncabezadoController::class, 'index'])->middleware('auth:sanctum');
+
 
 // habilitar el cors en ciertas rutas
 // Route::middleware(['cors'])->group(function () {
