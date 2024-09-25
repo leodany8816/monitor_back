2f2d6f14928c93cf3d7e64694d768d8a2ada3b0c
Leonardo Daniel Carranza García <leodany8816@gmail.com>
2024-09-24 21:11:21 -0600

    Una vez mas el cors a ver si ya es el ultimo

--- config/cors.php
+++ config/cors.php
@@ -21,9 +21,8 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => [env('FRONTEND_URL', '*')],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:4321')],
-    // 'allowed_origins' => ['https://bekaert.grupo-citi.com'],
-    //'allowed_origins' => [env('FRONTEND_URL', 'https://bekaert.grupo-citi.com/')],
+    //'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:4321')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://bekaert.grupo-citi.com')],
 
     'allowed_origins_patterns' => [],
 
