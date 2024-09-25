106260219f817703fd4245f3a2fdb87e09ae9e01
Leonardo Daniel Carranza García <leodany8816@gmail.com>
2024-09-25 17:41:00 -0600

    Configuracion token CSRF

--- config/cors.php
+++ config/cors.php
@@ -15,14 +15,15 @@ return [
     |
     */
 
-    'paths' => ['*'],
-    //'paths' => ['api/*'],
+    //'paths' => ['*'],
+    'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => [env('FRONTEND_URL', '*')],
     //'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:4321')],
-    'allowed_origins' => [env('FRONTEND_URL', 'https://bekaert.grupo-citi.com')],
+    //'allowed_origins' => [env('FRONTEND_URL', 'https://bekaert.grupo-citi.com')],
+    'allowed_origins' => ['https://bekaert.grupo-citi.com'],
 
     'allowed_origins_patterns' => [],
 
