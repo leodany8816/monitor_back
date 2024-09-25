ae8de46a161da7a187455f657bc86061ef472222
Leonardo Daniel Carranza García <leodany8816@gmail.com>
2024-09-24 20:44:39 -0600

    cors por una vez mas

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,7 @@ return [
 
     // 'allowed_origins' => [env('FRONTEND_URL', '*')],
     // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:4321/')],
-    'allowed_origins' => [env('FRONTEND_URL', 'bekaert.grupo-citi.com')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://bekaert.grupo-citi.com/')],
 
     'allowed_origins_patterns' => [],
 
