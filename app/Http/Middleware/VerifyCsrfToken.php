ebc23bbe62bd9bc38dfd0cfbf32125fab5ee242e
Leonardo Daniel Carranza García <leodany8816@gmail.com>
2024-09-26 01:49:38 -0600

    aver que ocurre

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'api/login',
+        'https://api.grupo-citi.com/api/login',
     ];
 }
